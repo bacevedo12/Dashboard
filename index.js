906d2b72afb8f3215efc1c00ce2b025c23493179
bacevedo12 <bacevedo85@hotmail.com>
2022-10-15 19:22:28 -0300

    Probando

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ chartUf();
 
 import {chartEuro} from './euro.js';
 chartEuro();
+
+// (()=>{
+//     const tabla = document.getElementById('valorDolar');
+//     const peticion = async (url) => {
+     
+//       const data = await axios.get(url)
+//       .then((respuesta) => {
+//         const lista = respuesta.data.serie;
+//         lista.filter(element => {
+//           tabla.innerHTML += 
+//           `
+//            <p>${element.fecha [0]}</p>
+//             <p>${element.valor[0]}</p>
+//           </p>`
+//         });
+//       });
+//     }
+//     const url = 'https://mindicador.cl/api/dolar';
+//     peticion(url);
+  
+//   })();
