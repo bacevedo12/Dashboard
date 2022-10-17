5b97f7a9e90275522188a07b827eb7422bd84f2e
Camila-FR <camilafloresramirez@gmail.com>
2022-10-17 17:04:39 -0300

    se incorporan graficos imacec e ipc

---
+++ ipc.js
@@ -0,0 +1,46 @@
+function chartIpc() {
+    const peticion5 = async (url) => {
+     
+      const data5 = await axios.get(url)
+      .then((respuesta) => {
+        const datos5 = respuesta.data.serie;
+        console.log(datos5)
+        let valores = datos5.map(element => element.valor);
+        let fechas = datos5.map(element =>{
+          let resultadoFecha5 = element.fecha.split('T');
+          resultadoFecha5 = resultadoFecha5[0].split('-').reverse().join('-');
+           return resultadoFecha5;
+        });
+        console.log(valores)
+        console.log(fechas)
+      charts5(fechas, valores); 
+      });
+    }
+    const url = 'https://mindicador.cl/api/ipc';
+    peticion5(url);
+  
+  };
+  
+  
+  function charts5 (fechas, valores){
+  const ctx = document.getElementById('myChartipc').getContext('2d');
+  const myChart5 = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: fechas.reverse(),
+          datasets: [{
+              label: 'IPC',
+              data: valores.reverse(),
+              borderColor:'red',
+              borderWidth: 1
+          }]
+      },
+  });
+  }
+
+
+  chartIpc()
+
+
+
+
