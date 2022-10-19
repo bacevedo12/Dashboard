e39d3c0dcf841916e51b3e122644b380525ecf06
Camila-FR <camilafloresramirez@gmail.com>
2022-10-19 15:13:54 -0300

    actualizacion de filtros

--- assets/js/imacec.js
+++ assets/js/imacec.js
@@ -1,22 +1,25 @@
-export function chartImacec() {
+let myChart6
+
+export function chartImacec(anio) {
     const peticion6 = async (url) => {
      
       const data6 = await axios.get(url)
       .then((respuesta) => {
         const datos6 = respuesta.data.serie;
-        console.log(datos6)
         let valores = datos6.map(element => element.valor);
         let fechas = datos6.map(element =>{
           let resultadoFecha6 = element.fecha.split('T');
           resultadoFecha6 = resultadoFecha6[0].split('-').reverse().join('-');
            return resultadoFecha6;
         });
-        console.log(valores)
-        console.log(fechas)
       charts6(fechas, valores); 
       });
     }
-    const url = 'https://mindicador.cl/api/imacec';
+    let url = 'https://mindicador.cl/api/imacec';
+    if (anio){
+      myChart6.destroy();
+      url = 'https://mindicador.cl/api/imacec/'+ anio;
+   }
     peticion6(url);
   
   };
@@ -24,7 +27,7 @@ export function chartImacec() {
   
   function charts6 (fechas, valores){
   const ctx = document.getElementById('myChart6').getContext('2d');
-  const myChart6 = new Chart(ctx, {
+  myChart6 = new Chart(ctx, {
       type: 'bar',
       data: {
           labels: fechas.reverse(),
@@ -49,6 +52,7 @@ export function chartImacec() {
         }
       },
   });
+  return myChart6;
   }
 
   // chartImacec()
